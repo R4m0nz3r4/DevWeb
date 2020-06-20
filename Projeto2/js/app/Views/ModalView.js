061954c2f2c6beac2cbb8ec753c44975a40d8939
R4m0nz3r4 <ramonfersouza@gmail.com>
2020-06-20 10:08:09 -0300

    Título do modal

--- Projeto2/js/app/Views/ModalView.js
+++ Projeto2/js/app/Views/ModalView.js
@@ -9,7 +9,7 @@ class ModalView extends View{
                 <div class='modal-dialog modal-dialog-centered' role='document'>
                     <div class='modal-content'>
                         <div class='modal-header'>
-                            <h5 class='modal-title'>${ model.editavel ? "Editar Info. Pessoa" : "Vsializar Info Pessoa" }</h5>
+                            <h5 class='modal-title'>Informações Pessoais</h5>
                             <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
                                 <span aria-hidden='true'>&times;</span>
                             </button>
